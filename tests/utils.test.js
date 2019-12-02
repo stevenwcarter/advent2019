b6bc6c48a3b47f5e6109c1e828e6b8d7f06c11ca
Steven Carter <steven.carter@bounteous.com>
2019-12-02 00:32:31 -0500

    Day 2

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,5 +1,9 @@
 import * as Utils from '../utils';
 
+import { intCodeSolver } from '../utils';
+
+
+
 describe('Utils', () => {
     describe('readInputAsNumbers', () => {
         it('should handle blank lines', () => {
@@ -33,6 +37,21 @@ describe('Utils', () => {
             3
             4
             `)).toEqual(["a","1","2","3","4"]);
+        });
+    });
+
+    describe('intCodeSolver', () => {
+        it('should calculate intCodes correctly (example 1)', () => {
+            expect(intCodeSolver([1,0,0,0,99], 0)).toEqual([2,0,0,0,99]);
+        });
+        it('should calculate intCodes correctly (example 2)', () => {
+            expect(intCodeSolver([2,3,0,3,99], 0)).toEqual([2,3,0,6,99]);
+        });
+        it('should calculate intCodes correctly (example 3)', () => {
+            expect(intCodeSolver([2,4,4,5,99,0], 0)).toEqual([2,4,4,5,99,9801]);
+        });
+        it('should calculate intCodes correctly (example 4)', () => {
+            expect(intCodeSolver([1,1,1,4,99,5,6,0,99], 0)).toEqual([30,1,1,4,2,5,6,0,99]);
         });
     });
 });
--- utils.js
+++ utils.js
@@ -2,3 +2,22 @@ import _ from 'lodash';
 
 export const readInputAsNumbers = (input) => input.split(/\r|\n|\r\n/).map(a => parseInt(_.trim(a), 10)).filter(a => _.isNumber(a) && !_.isNaN(a));
 export const readInputAsStrings = (input) => input.split(/\r|\n|\r\n/).map(_.trim).filter(a => !_.isEmpty(a));
+
+export const intCodeSolver = (input, pos) => {
+    const opCode = input[pos];
+    const operand1 = input[input[pos+1]];
+    const operand2 = input[input[pos+2]];
+    const outputPos = input[pos+3];
+
+    if (opCode === 99) {
+        return input;
+    } else if (opCode === 1) {
+        input[outputPos] = operand1 + operand2;
+    } else if (opCode === 2) {
+        input[outputPos] = operand1 * operand2;
+    } else {
+        console.error("should not have happened", opCode);
+    }
+
+    return intCodeSolver(input, pos+4);
+};
