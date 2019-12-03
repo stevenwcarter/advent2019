213a63e84a0c673cd93c5a0459afa3a31692453c
Steven Carter <steven.carter@bounteous.com>
2019-12-03 00:51:39 -0500

    Day 3

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 export const readInputAsNumbers = (input) => input.split(/\r|\n|\r\n/).map(a => parseInt(_.trim(a), 10)).filter(a => _.isNumber(a) && !_.isNaN(a));
+
 export const readInputAsStrings = (input) => input.split(/\r|\n|\r\n/).map(_.trim).filter(a => !_.isEmpty(a));
 
 export const intCodeSolver = (input, pos) => {
